39f1788c60d98ff28081b3cc57f3ee2419acf5a3
davidb75016 <david.bolton@orange.fr>
2018-05-10 19:48:10 +0200

    update

--- src/assets/main.js
+++ src/assets/main.js
@@ -3,7 +3,12 @@ let attempt = document.getElementById('attempt');
 
 function guess() {
     let input = document.getElementById('user-guess');
-    //add functionality to guess function here
+    if(answer.value == "" || attempt.value == ""){
+      setHiddenFields();
+    }
+    if(validateInput(input.value) == false){
+      return false;
+    } else { attempt.value++;}
 }
 
 //implement new functions here
@@ -11,6 +16,17 @@ function setHiddenFields(){
   answer.value = Math.floor(Math.random() * 10000).toString();
   while (answer.value.length < 4){
     answer.value = "0"+ answer.value;
+    attempt.value = "0";
   }
-  }
+}
+
+function setMessage(message){
+  document.getElementById('message').innerHTML = message;
+}
+
+function validateInput(input){
+  if (input.length != 4){
+    setMessage("Guesses must be exactly 4 characters long.");
+    return false;
+  } else {return true;}
 }
